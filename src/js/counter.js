4f397ffda13c773d732d77f1cffec9c1096b5369
miguel-angel-git <miguelangelgit@gmail.com>
2025-08-04 23:03:14 +0200

    update 1.0

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,35 @@
-function add(){
+
+let numero = 0;
+
+function update() {
+    document.getElementById("number").textContent = numero;
+}
+
+function leerNumero() {
+    let NumeroTemp = document.getElementById("number").textContent;
+    numero = parseInt(NumeroTemp) || 0;
+}
+
+function add() {
     //Escribir tu código que aumenta
+    leerNumero();
+    numero++;
+    update();
 }
 
-function reset(){
+function reset() {
     //Escribir tu código que hace un reset a 0
+    leerNumero();
+    numero = 0;
+    update();
 }
 
-function decrease(){
+function decrease() {
     //Escribir tu código que resta
+    leerNumero();
+    numero--;
+    update();
 };
 
-export {add, reset, decrease}
+export { add, reset, decrease }
+
--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,7 @@
-import {add, reset, decrease} from './counter.js'
+import { add, reset, decrease } from './counter.js'
 
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+document.getElementById("add").addEventListener("click", add);
+document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("decrease").addEventListener("click", decrease);
